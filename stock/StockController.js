643f708f5458f308c7a87a2f869537d8cd20dc18
sjoseph1848 <sjoseph1848@gmail.com>
2020-07-27 23:33:08 -0500

    gave the api a parameter

--- stock/StockController.js
+++ stock/StockController.js
@@ -9,16 +9,21 @@ const { route } = require('../app');
 const finKey = process.env.FIN_KEY;
 
 
-router.get('/', function (req, res) {
-    const stocks = getStocks();
+router.get('/:stock', function (req, res) {
+    
+    const stock = req.params.stock;
+    const stocks = getStocks(stock);
     stocks.then(response => res.status(200).send(response.data))
         .catch(error => console.log(error));
 
 })
 
-const getStocks = async () => {
+
+
+
+const getStocks = async (stock) => {
     try {
-        return await axios.get(`https://financialmodelingprep.com/api/v3/profile/AAPL?apikey=${finKey}`)
+        return await axios.get(`https://financialmodelingprep.com/api/v3/profile/${stock}?apikey=${finKey}`)
     } catch (error) {
         console.error(error)
     }
