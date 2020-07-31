7074d39e9c0ff7907c4d77a180ce0781629835db
sjoseph1848 <sjoseph1848@gmail.com>
2020-07-30 22:17:43 -0500

    added more cors

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();
 const db = require('./db');
 
 const StockController = require('./stock/StockController');
+const cors = require('cors');
+app.use(cors());
 app.use('/stocks', StockController);
 
 module.exports = app;
